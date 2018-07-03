03d030ff6cb4bcead9085f6b458ce947875a79ef
NHibiki <i@yuuno.cc>
2018-07-03 13:13:52 +0800

    Switch BMOB to OD

--- functions/bmob.js
+++ functions/bmob.js
@@ -1,72 +1,142 @@
 const https = require('https');
 
-const APPID = process.env.bmob_for_use_app || "";
-const APPSC = process.env.bmob_for_use_sec || "";
+const siteId = "";
 
-function request (uri, query={}, method='GET', server=1, addsHeaders={}, callback=()=>{}) {
+function request (host, uri, query={}, method='GET', headers={}, callback=()=>{}) {
 
-    let queryData = '';
-    if (method === 'GET') {
-        for (let key in query) {
-            queryData += `&${key}=${query[key]}`;
+    return new Promise((resolve, reject) => {
+
+        let postData = "";
+        if (method !== 'GET') {
+            if (typeof query === 'object' && !Buffer.isBuffer(query)) {
+                if (headers["Content-Type"] && headers["Content-Type"].includes("json")) {
+                    postData = JSON.stringify(query);
+                } else {
+                    postData = "";
+                    for (let key in query) {
+                        postData += `&${key}=${escape(query[key])}`;
+                    }
+                    postData = postData.substr(1);
+                }
+            } else postData = query;
         }
-        if (queryData) queryData = encodeURI('?' + queryData.substr(1));
-    }
 
-    let option = {
-        host: 'api.bmob.cn',
-        port: 443,
-        path: `/${server}${uri}${queryData}`,
-        headers: {
-            'X-Bmob-Application-Id': APPID,
-            'X-Bmob-REST-API-Key': APPSC,
-            ...addsHeaders
-        },
-        method
-    }
+        let queryData = '';
+        if (method === 'GET') {
+            if (typeof query === 'object') {
+                for (let key in query) {
+                    queryData += `&${key}=${query[key]}`;
+                }
+                if (queryData) queryData = encodeURI('?' + queryData.substr(1));
+            } else {queryData = encodeURI(query);}
+        }
 
-    let req = https.request(option, function (res) {
-        res.setEncoding('utf-8');
-        let html = "";
-        res.on('data', function(data) { html += data; });
-        res.on('end', function() {
-            try {
-                let data = JSON.parse(html); callback(data);
-            } catch (error) {
-                callback(null);
+        let option = {
+            host,
+            port: 443,
+            path: `${uri}${queryData}`,
+            headers : {
+                "Connection": "keep-alive",
+                "User-Agent": "SlinkClient/1.0",
+                "Content-Length": postData.length,
+                ...headers
+            },
+            method
+        }
+
+        let req = https.request(option, function (res) {
+            res.setEncoding('utf-8');
+            let html = "";
+            res.on('data', function(data) { html += data; });
+            res.on('end', function() {
+                try {
+                    let data = JSON.parse(html); callback(data); resolve(data);
+                } catch (error) {
+                    callback(null); reject(error);
+                }
+            });
+        });
+        req.on('error', e => {
+            callback(null); reject(e);
+        });
+
+        if (method !== "GET") req.write(postData);
+        req.end();
+
+    });
+
+}
+
+function mrequest(uri, query, key, method='GET', headers={}) {
+    return request("graph.microsoft.com", "/v1.0/me/drive/root" + uri, query, method, {
+        "Authorization": "bearer " + key,
+        ...headers
+    });
+}
+
+function getKey (callback) {
+
+    request(siteId, "/slinkkey/moenya/odrefresh,odtoken,odexpire,clientid,redirecturi,clientsc").then(data => {
+        let expire = (parseInt(data.odexpire, 10) || 0) < ((new Date()).getTime() / 1000);
+        if (!expire) { callback(data.odtoken); return; }
+        request("login.microsoftonline.com", "/common/oauth2/v2.0/token", {
+            client_id: data.clientid,
+            redirect_uri: data.redirecturi,
+            client_secret: data.clientsc,
+            refresh_token: data.odrefresh,
+            grant_type: "refresh_token"
+        }, "POST", { "Content-Type": "application/x-www-form-urlencoded" }).then(res => {
+            if (res["access_token"] && res["refresh_token"] && res["expires_in"]) {
+                request(siteId, "/slinkkey/moenya/odrefresh,odtoken,odexpire", {
+                    odrefresh: res["refresh_token"],
+                    odtoken: res["access_token"],
+                    odexpire: (parseInt(res["expires_in"], 10) + parseInt((new Date()).getTime() / 1000, 10) - 60)
+                }, "POST", { "Content-Type": "application/x-www-form-urlencoded" }).then(ans => {
+                    callback(res["access_token"]);
+                });
             }
         });
-    });
-    req.on('error', e => {
-        callback(null);
-    });
+    })
 
-    if (method !== 'GET') {
-        if (server === 1) req.write(JSON.stringify(query));
-        else req.write(query);
+}
+
+function parseData(data) {
+    if (!data.error) {
+        let url = data["@microsoft.graph.downloadUrl"];
+        let time = parseInt((new Date(data["createdDateTime"])).getTime() / 1000, 10);
+        let name = data["name"];
+        let size = data["size"];
+        let type = data["file"]["mimeType"];
+        let hash = data["file"]["hashes"]["sha1Hash"];
+        return {url, time, name, size, type, hash};
+    } else {
+        return null;
     }
-    req.end();
-
 }
 
-function retrieveURL (fid, callback) {
-    request("/classes/moenya", {"where": `{"fid":"${fid}"}`}, 'GET', 1, {}, data => {
-        let result = data ? data.results[0] : null;
-        callback((result ? result.url : '').replace("http://", "https://"));
-    });
-}
-
-function saveFile (info, content, callback) {
-    request(`/files/${info.name}`, content, 'POST', 2, {"Content-Type": info.ctype}, data => {
-        let {cdn, url} = data;
-        let fid = info.lid;
-        request("/classes/moenya", {fid, url, cdn}, 'POST', 1, {"Content-Type": "application/json"}, res => {
-            callback(res);
+function getFile(path) {
+    return new Promise((resolve) => {
+        getKey(token => {
+            mrequest(`:/upload${path}`, {}, token).then(data => {
+                resolve(parseData(data));
+            })
         });
-    });
+    })
 }
 
-module.exports = {
-    retrieveURL,
-    saveFile
+function saveFile(path, content, type) {
+    return new Promise((resolve) => {
+        getKey(token => {
+            mrequest(`:/upload${path}:/content`, content, token, 'PUT', {"Content-Type": type}).then(data => {
+                resolve(parseData(data));
+            });
+        });
+    })
 }
+
+// getFile("/favicon.png").then(res => {
+//     console.log(res);
+// })
+// saveFile("/3jeA8fu3/favicon.png", require("fs").readFileSync("../favicon.png"), "image/png").then(res => {
+//     console.log(res);
+// })
--- functions/slink.js
+++ functions/slink.js
@@ -1,86 +1,144 @@
 var https = require('https');
 var crypto = require('crypto');
 
-const APPID = process.env.bmob_for_use_app || "";
-const APPSC = process.env.bmob_for_use_sec || "";
-
+let siteId = process.env.site_for_use || "";
+let VERBOSE = process.env.site_for_verbose || "0";
 const log = function(...text) {
-    console.log(`[MB ${parseInt((new Date()).getTime() / 1000, 10)}]`, ...text)
+    if (VERBOSE !== "0") console.log(`[MB ${parseInt((new Date()).getTime() / 1000, 10)}]`, ...text)
 }
-
-function request (uri, query={}, method='GET', server=1, addsHeaders={}, callback=()=>{}) {
-
-    let queryData = '';
-    if (method === 'GET') {
-        for (let key in query) {
-            queryData += `&${key}=${query[key]}`;
-        }
-        if (queryData) queryData = encodeURI('?' + queryData.substr(1));
-    }
-
-    let option = {
-        host: 'api.bmob.cn',
-        port: 443,
-        path: `/${server}${uri}${queryData}`,
-        headers: {
-            'X-Bmob-Application-Id': APPID,
-            'X-Bmob-REST-API-Key': APPSC,
-            ...addsHeaders
-        },
-        method
-    }
-
-    let req = https.request(option, function (res) {
-        res.setEncoding('utf-8');
-        let html = "";
-        res.on('data', function(data) { html += data; });
-        res.on('end', function() {
-            try {
-                let data = JSON.parse(html); callback(data);
-            } catch (error) {
-                callback(null);
-            }
-        });
-    });
-    req.on('error', e => {
-        callback(null);
-    });
-
-    if (method !== 'GET') {
-        if (server === 1) req.write(JSON.stringify(query));
-        else req.write(query);
-    }
-    req.end();
-
-}
-
-function retrieveURL (fid, callback) {
-    request("/classes/moenya", {"where": `{"fid":"${fid}"}`}, 'GET', 1, {}, data => {
-        let result = data ? data.results[0] : null;
-        callback((result ? result.url : '').replace("http://", "https://"));
-    });
-}
-
-function saveFile (info, content, callback) {
-    request(`/files/${encodeURI(info.name)}`, content, 'POST', 2, {"Content-Type": info.ctype}, data => {
-        let {cdn, url} = data;
-        let fid = info.lid;
-        request("/classes/moenya", {fid, url, cdn}, 'POST', 1, {"Content-Type": "application/json"}, res => {
-            callback(res);
-        });
-    });
-}
-
 function md5 (text) {
   return crypto.createHash('md5').update(text).digest('hex');
 };
-
 const min = (a, b) => a > b ? b : a;
-const exp = /^(\w+):\/\/([^\/:]*)(?::(\d+))?(\/.*)/;
+function request (host, uri, query={}, method='GET', headers={}, callback=()=>{}) {
+
+    return new Promise((resolve, reject) => {
+
+        let postData = "";
+        if (method !== 'GET') {
+            if (typeof query === 'object' && !Buffer.isBuffer(query)) {
+                if (headers["Content-Type"] && headers["Content-Type"].includes("json")) {
+                    postData = JSON.stringify(query);
+                } else {
+                    postData = "";
+                    for (let key in query) {
+                        postData += `&${key}=${escape(query[key])}`;
+                    }
+                    postData = postData.substr(1);
+                }
+            } else postData = query;
+        }
+
+        let queryData = '';
+        if (method === 'GET') {
+            if (typeof query === 'object') {
+                for (let key in query) {
+                    queryData += `&${key}=${query[key]}`;
+                }
+                if (queryData) queryData = encodeURI('?' + queryData.substr(1));
+            } else {queryData = encodeURI(query);}
+        }
+
+        let option = {
+            host,
+            port: 443,
+            path: `${uri}${queryData}`,
+            headers : {
+                "Connection": "keep-alive",
+                "User-Agent": "SlinkClient/1.0",
+                "Content-Length": postData.length,
+                ...headers
+            },
+            method
+        }
+
+        let req = https.request(option, function (res) {
+            res.setEncoding('utf-8');
+            let html = "";
+            res.on('data', function(data) { html += data; });
+            res.on('end', function() {
+                try {
+                    let data = JSON.parse(html); callback(data); resolve(data);
+                } catch (error) {
+                    callback(null); reject(error);
+                }
+            });
+        });
+        req.on('error', e => {
+            callback(null); reject(e);
+        });
+
+        if (method !== "GET") req.write(postData);
+        req.end();
+
+    });
+
+}
+function mrequest(uri, query, key, method='GET', headers={}) {
+    return request("graph.microsoft.com", "/v1.0/me/drive/root" + uri, query, method, {
+        "Authorization": "bearer " + key,
+        ...headers
+    });
+}
+function getKey (callback) {
+
+    request(siteId, "/slinkkey/moenya/odrefresh,odtoken,odexpire,clientid,redirecturi,clientsc").then(data => {
+        let expire = (parseInt(data.odexpire, 10) || 0) < ((new Date()).getTime() / 1000);
+        if (!expire) { callback(data.odtoken); return; }
+        request("login.microsoftonline.com", "/common/oauth2/v2.0/token", {
+            client_id: data.clientid,
+            redirect_uri: data.redirecturi,
+            client_secret: data.clientsc,
+            refresh_token: data.odrefresh,
+            grant_type: "refresh_token"
+        }, "POST", { "Content-Type": "application/x-www-form-urlencoded" }).then(res => {
+            if (res["access_token"] && res["refresh_token"] && res["expires_in"]) {
+                request(siteId, "/slinkkey/moenya/odrefresh,odtoken,odexpire", {
+                    odrefresh: res["refresh_token"],
+                    odtoken: res["access_token"],
+                    odexpire: (parseInt(res["expires_in"], 10) + parseInt((new Date()).getTime() / 1000, 10) - 60)
+                }, "POST", { "Content-Type": "application/x-www-form-urlencoded" }).then(ans => {
+                    callback(res["access_token"]);
+                });
+            }
+        });
+    })
+
+}
+function parseData(data) {
+    if (!data.error) {
+        let url = data["@microsoft.graph.downloadUrl"];
+        let time = parseInt((new Date(data["createdDateTime"])).getTime() / 1000, 10);
+        let name = data["name"];
+        let size = data["size"];
+        let type = data["file"]["mimeType"];
+        let hash = data["file"]["hashes"]["sha1Hash"];
+        return {url, time, name, size, type, hash};
+    } else {
+        return null;
+    }
+}
+function getFile(path) {
+    return new Promise((resolve) => {
+        getKey(token => {
+            mrequest(`:/upload${path}`, {}, token).then(data => {
+                resolve(parseData(data));
+            })
+        });
+    })
+}
+function saveFile(path, content, type) {
+    return new Promise((resolve) => {
+        getKey(token => {
+            mrequest(`:/upload${path}:/content`, content, token, 'PUT', {"Content-Type": type}).then(data => {
+                resolve(parseData(data));
+            });
+        });
+    })
+}
+
 
 function getSlink (info, callback) {
-
-    let siteId = process.env.site_for_use || "";
 
     var postData = `data=${JSON.stringify(info)}`;
     var options = {
@@ -144,20 +202,20 @@ exports.handler = function (event, context, callback) {
     let ip = event.headers["x-forwarded-for"] || "";
     let size = content.length;
 
-    let lid = md5(`saveFile/${size}SFN${name}SlinkFile${type}${time}`);
-
     let recordedData = {
-        name, size, type, ctype, ip, lid, time
+        name, size, type, ctype, ip, time
     };
 
     log("Done Converting From", ip);
 
-    saveFile(recordedData, content, res => {
+    getSlink(recordedData, slink => {
 
-        log("Done Saving From", ip);
-        getSlink(recordedData, slink => {
+        log("Done Getting Slink", ip);
 
-            log("All Done From", ip);
+        saveFile(`/${slink}/${name}`, content, ctype).then(res => {
+
+            log("Done Saving", ip);
+
             callback(null, {
                 statusCode: 200,
                 headers: {
@@ -167,7 +225,9 @@ exports.handler = function (event, context, callback) {
                 },
                 body: JSON.stringify({status: true, data: {...recordedData, url: `https://file.yuuno.cc/${slink}`, identifier: slink, date: time}})
             });
+
         });
+
     });
 
 }
